ffeb80de795b9079eb997b35b96101949d597c9a
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-03-03 00:15:56 +0900

    add some traits

---
+++ string/rolling_hash/src/lib.rs
@@ -0,0 +1,151 @@
+use modint_traits::{FromPrimitiveInt, ModInt};
+use std::fmt::Display;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ModIntMersenne {
+    value: u64,
+}
+
+impl Display for ModIntMersenne {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+impl ModIntMersenne {
+    pub fn value(&self) -> u64 {
+        self.value
+    }
+    pub fn modulus() -> u64 {
+        MOD
+    }
+}
+
+const MOD: u64 = (1 << 61) - 1;
+
+impl ModInt for ModIntMersenne {
+    fn pow(&self, n: u64) -> Self {
+        todo!()
+    }
+    fn inv(&self) -> Self {
+        todo!()
+    }
+}
+
+impl FromPrimitiveInt<u32> for ModIntMersenne {
+    type Output = ModIntMersenne;
+    fn new(x: u32) -> Self::Output {
+        ModIntMersenne { value: x.into() }
+    }
+}
+
+impl FromPrimitiveInt<u64> for ModIntMersenne {
+    type Output = ModIntMersenne;
+    fn new(x: u64) -> Self::Output {
+        let tmp = (x >> 61) + (x & MOD);
+        ModIntMersenne {
+            value: if tmp >= MOD { tmp - MOD } else { tmp },
+        }
+    }
+}
+
+impl FromPrimitiveInt<i32> for ModIntMersenne {
+    type Output = ModIntMersenne;
+    fn new(x: i32) -> Self {
+        let x_abs = x.abs() as u64;
+        -ModIntMersenne::new(x_abs)
+    }
+}
+
+impl FromPrimitiveInt<i64> for ModIntMersenne {
+    type Output = ModIntMersenne;
+    fn new(x: i64) -> Self {
+        if x == i64::MIN {
+            -ModIntMersenne::new((x + (MOD as i64)).abs() as u64)
+        } else if x < 0 {
+            -ModIntMersenne::new(x.abs() as u64)
+        } else {
+            ModIntMersenne::new(x as u64)
+        }
+    }
+}
+
+impl Neg for ModIntMersenne {
+    type Output = Self;
+    fn neg(self) -> Self {
+        if self.value == 0 {
+            self
+        } else {
+            ModIntMersenne {
+                value: MOD - self.value,
+            }
+        }
+    }
+}
+
+impl AddAssign for ModIntMersenne {
+    fn add_assign(&mut self, rhs: Self) {
+        self.value += rhs.value;
+        if self.value >= MOD {
+            self.value -= MOD;
+        }
+    }
+}
+
+impl Add for ModIntMersenne {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        let mut tmp = self;
+        tmp += rhs;
+        tmp
+    }
+}
+
+impl SubAssign for ModIntMersenne {
+    fn sub_assign(&mut self, rhs: Self) {
+        if self.value < rhs.value {
+            self.value += MOD;
+        }
+        self.value -= rhs.value;
+    }
+}
+
+impl Sub for ModIntMersenne {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        let mut tmp = self;
+        tmp -= rhs;
+        tmp
+    }
+}
+
+impl MulAssign for ModIntMersenne {
+    fn mul_assign(&mut self, rhs: Self) {
+        todo!()
+    }
+}
+
+impl Mul for ModIntMersenne {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        let mut tmp = self;
+        tmp *= rhs;
+        tmp
+    }
+}
+
+impl DivAssign for ModIntMersenne {
+    fn div_assign(&mut self, rhs: Self) {
+        todo!()
+    }
+}
+
+impl Div for ModIntMersenne {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        let mut tmp = self;
+        tmp /= rhs;
+        tmp
+    }
+}
---
+++ traits/modint_traits/src/lib.rs
@@ -0,0 +1,49 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::fmt::{Debug, Display};
+
+pub trait ModInt:
+    Debug
+    + Clone
+    + PartialEq
+    + Eq
+    + Display
+    + Copy
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + AddAssign
+    + SubAssign
+    + MulAssign
+    + DivAssign
+    + Neg<Output = Self>
+{
+    fn pow(&self, n: u64) -> Self;
+    fn inv(&self) -> Self;
+}
+
+/// u32 or u64 for Mod
+pub trait ModType {}
+impl ModType for u32 {}
+impl ModType for u64 {}
+
+/// Trait for primitive integer types.
+pub trait Number {}
+impl Number for i8 {}
+impl Number for i16 {}
+impl Number for i32 {}
+impl Number for i64 {}
+impl Number for i128 {}
+impl Number for isize {}
+impl Number for u8 {}
+impl Number for u16 {}
+impl Number for u32 {}
+impl Number for u64 {}
+impl Number for u128 {}
+impl Number for usize {}
+
+/// Trait for converting primitive integers to ModInt.
+pub trait FromPrimitiveInt<T: Number> {
+    type Output;
+    fn new(x: T) -> Self::Output;
+}
