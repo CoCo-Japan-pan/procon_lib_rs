b8915dd2c4562f95e876cefbcfc31f5c3acf6cf9
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-03-03 21:45:42 +0900

    add an empty explain of dual segtree

--- crates/data_structure/dual_seg_tree/src/lib.rs
+++ crates/data_structure/dual_seg_tree/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// 可換な作用素を通常のセグメント木のように持つ
+/// 実際に値を取得するときに適用する
+pub struct DualSegTree {}
