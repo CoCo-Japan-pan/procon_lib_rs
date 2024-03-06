8b65b3d88a45cdf5c4b6c677fc4bfeb84d44240c
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-03-06 18:58:40 +0900

    verify commutative lazy seg

---
+++ verify/AOJ/dsl_2h_lazy_seg_commutative/src/main.rs
@@ -0,0 +1,65 @@
+use algebra::CommutativeMapMonoid;
+use lazy_seg_tree::LazySegTree;
+use proconio::{fastout, input};
+
+struct MinMonoid {}
+impl algebra::Monoid for MinMonoid {
+    type S = i32;
+    fn id_element() -> Self::S {
+        i32::MAX
+    }
+    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+        *a.min(b)
+    }
+}
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct AddMap {
+    add_val: i32,
+}
+impl algebra::Map for AddMap {
+    type Target = i32;
+    fn id_map() -> Self {
+        AddMap { add_val: 0 }
+    }
+    fn composition(&mut self, rhs: &Self) {
+        self.add_val += rhs.add_val;
+    }
+    fn mapping(&self, target: &Self::Target) -> Self::Target {
+        target + self.add_val
+    }
+}
+struct RmqRaq {}
+impl algebra::MapMonoid for RmqRaq {
+    type M = MinMonoid;
+    type F = AddMap;
+}
+impl CommutativeMapMonoid for RmqRaq {}
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+    let mut lazy_seg = LazySegTree::<RmqRaq>::from(vec![0; n]);
+    for _ in 0..q {
+        input! {
+            com: usize,
+        }
+        if com == 0 {
+            input! {
+                s: usize,
+                t: usize,
+                x: i32,
+            }
+            let map = AddMap { add_val: x };
+            lazy_seg.apply_range_commutative(s..=t, &map);
+        } else {
+            input! {
+                s: usize,
+                t: usize,
+            }
+            println!("{}", lazy_seg.prod(s..=t));
+        }
+    }
+}
