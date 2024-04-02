03e6031d2a3c8a0b4a688f0d973bb66cc54b1ba5
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-04-02 11:54:48 +0900

    flatten to 1d

--- crates/data_structure/seg_tree_2d/src/lib.rs
+++ crates/data_structure/seg_tree_2d/src/lib.rs
@@ -14,7 +14,13 @@ pub struct SegTree2D<M: Monoid> {
     ceil_log_w: usize,
     leaf_height: usize,
     leaf_width: usize,
-    data: Vec<Vec<M::Target>>,
+    data: Vec<M::Target>,
+}
+
+macro_rules! index {
+    ($self: expr, $h:expr, $w:expr) => {
+        $h * $self.leaf_width * 2 + $w
+    };
 }
 
 impl<M: Monoid> From<&Vec<Vec<M::Target>>> for SegTree2D<M> {
@@ -25,9 +31,10 @@ impl<M: Monoid> From<&Vec<Vec<M::Target>>> for SegTree2D<M> {
         let ceil_log_w = (32 - (width as u32).saturating_sub(1).leading_zeros()) as usize;
         let leaf_height = 1 << ceil_log_h;
         let leaf_width = 1 << ceil_log_w;
-        let mut data = vec![vec![M::id_element(); leaf_width * 2]; leaf_height * 2];
-        for h in 0..height {
-            data[leaf_height + h][leaf_width..leaf_width + width].clone_from_slice(&v[h]);
+        let mut data = vec![M::id_element(); leaf_width * leaf_height * 4];
+        for (h, v) in v.iter().enumerate() {
+            let base = (leaf_height + h) * leaf_width * 2 + leaf_width;
+            data[base..base + width].clone_from_slice(v);
         }
         let mut ret = SegTree2D {
             height,
@@ -61,7 +68,7 @@ impl<M: Monoid> SegTree2D<M> {
         assert!(h < self.height && w < self.width);
         let h = h + self.leaf_height;
         let w = w + self.leaf_width;
-        self.data[h][w] = x;
+        self.data[index!(self, h, w)] = x;
         for i in 1..=self.ceil_log_h {
             self.update_from_col_leaf(h >> i, w);
         }
@@ -77,11 +84,11 @@ impl<M: Monoid> SegTree2D<M> {
 
     pub fn get(&self, h: usize, w: usize) -> M::Target {
         assert!(h < self.height && w < self.width);
-        self.data[h + self.leaf_height][w + self.leaf_width].clone()
+        self.data[index!(self, h + self.leaf_height, w + self.leaf_width)].clone()
     }
 
     pub fn all_prod(&self) -> M::Target {
-        self.data[1][1].clone()
+        self.data[index!(self, 1, 1)].clone()
     }
 
     pub fn prod<R1: RangeBounds<usize>, R2: RangeBounds<usize>>(
@@ -125,12 +132,12 @@ impl<M: Monoid> SegTree2D<M> {
                 let mut r = w_right;
                 while l < r {
                     if l & 1 != 0 {
-                        ret = M::binary_operation(&ret, &self.data[h_left][l]);
+                        ret = M::binary_operation(&ret, &self.data[index!(self, h_left, l)]);
                         l += 1;
                     }
                     if r & 1 != 0 {
                         r -= 1;
-                        ret = M::binary_operation(&self.data[h_left][r], &ret);
+                        ret = M::binary_operation(&self.data[index!(self, h_left, r)], &ret);
                     }
                     l >>= 1;
                     r >>= 1;
@@ -143,12 +150,12 @@ impl<M: Monoid> SegTree2D<M> {
                 let mut r = w_right;
                 while l < r {
                     if l & 1 != 0 {
-                        ret = M::binary_operation(&ret, &self.data[h_right][l]);
+                        ret = M::binary_operation(&ret, &self.data[index!(self, h_right, l)]);
                         l += 1;
                     }
                     if r & 1 != 0 {
                         r -= 1;
-                        ret = M::binary_operation(&self.data[h_right][r], &ret);
+                        ret = M::binary_operation(&self.data[index!(self, h_right, r)], &ret);
                     }
                     l >>= 1;
                     r >>= 1;
@@ -163,10 +170,16 @@ impl<M: Monoid> SegTree2D<M> {
 
 impl<M: Monoid> SegTree2D<M> {
     fn update_from_col_leaf(&mut self, h: usize, w: usize) {
-        self.data[h][w] = M::binary_operation(&self.data[h * 2][w], &self.data[h * 2 + 1][w]);
+        self.data[index!(self, h, w)] = M::binary_operation(
+            &self.data[index!(self, h * 2, w)],
+            &self.data[index!(self, h * 2 + 1, w)],
+        );
     }
     /// colに比べてキャッシュ効率が良いので、こっちを多く使いたい
     fn update_from_row_leaf(&mut self, h: usize, w: usize) {
-        self.data[h][w] = M::binary_operation(&self.data[h][w * 2], &self.data[h][w * 2 + 1]);
+        self.data[index!(self, h, w)] = M::binary_operation(
+            &self.data[index!(self, h, w * 2)],
+            &self.data[index!(self, h, w * 2 + 1)],
+        );
     }
 }
