eeab0093dd88ccec76001699226f407eb32f76ef
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-04-04 00:53:08 +0900

    add another verify for hld

---
+++ verify/yosupo/vertex_add_path_sum/src/main.rs
@@ -0,0 +1,51 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/vertex_add_path_sum
+
+use fenwick_tree::FenwickTree;
+use hld::{Path, HLD};
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        a: [u64; n],
+        u_v: [(usize, usize); n - 1],
+    }
+    let mut graph = vec![vec![]; n];
+    for (u, v) in u_v {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+    let hld = HLD::new(&graph, 0);
+    let mut ft = FenwickTree::new(n, 0_u64);
+    for i in 0..n {
+        ft.add(hld.hld_in(i), a[i]);
+    }
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        if t == 0 {
+            input! {
+                p: usize,
+                x: u64,
+            }
+            ft.add(hld.hld_in(p), x);
+        } else {
+            input! {
+                u: usize,
+                v: usize,
+            }
+            let mut ans = 0;
+            for path in hld.path(u, v, true) {
+                match path {
+                    Path::Ascending(l, r) | Path::Descending(l, r) => {
+                        ans += ft.sum(l..r);
+                    }
+                }
+            }
+            println!("{}", ans);
+        }
+    }
+}
