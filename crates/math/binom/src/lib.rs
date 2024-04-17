c9c280651b85944913b5656caa85af3b2503a69b
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-04-17 21:52:17 +0900

    impl binom

--- crates/math/binom/src/lib.rs
+++ crates/math/binom/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use modint_traits::ModInt;
+
+pub struct Binom<T: ModInt> {
+    max_n: usize,
+    fact: Vec<T>,
+    ifact: Vec<T>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T: ModInt> Binom<T> {
+    pub fn new(max_n: usize) -> Self {
+        let mut fact = vec![T::raw(0); max_n + 1];
+        let mut ifact = vec![T::raw(0); max_n + 1];
+        fact[0] = T::raw(1);
+        for i in 1..=max_n {
+            fact[i] = fact[i - 1] * T::new(i as u64);
+        }
+        ifact[max_n] = fact[max_n].inv();
+        for i in (1..=max_n).rev() {
+            ifact[i - 1] = ifact[i] * T::new(i as u64);
+        }
+        Self { max_n, fact, ifact }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// nCkの計算(n<kの場合は0とする)
+    pub fn cmp(&self, n: usize, k: usize) -> T {
+        assert!(n <= self.max_n);
+        if n < k {
+            T::raw(0)
+        } else {
+            self.fact[n] * self.ifact[k] * self.ifact[n - k]
+        }
     }
 }
