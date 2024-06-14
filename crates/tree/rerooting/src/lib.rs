e6ac6398b6d59452f083459c5e3f6f6b0787103f
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-06-15 00:56:02 +0900

    edit some docs

--- crates/tree/rerooting/src/lib.rs
+++ crates/tree/rerooting/src/lib.rs
@@ -5,24 +5,24 @@
 use algebra::{Commutative, Monoid};
 
 #[derive(Debug)]
-pub struct Rerooting<T: Monoid + Commutative, F: FnMut(&T::Target, usize, usize) -> T::Target> {
+pub struct Rerooting<M: Monoid + Commutative, F: FnMut(&M::Target, usize, usize) -> M::Target> {
     vertex_cnt: usize,
     /// 根を0とした場合の各頂点を根とする部分木のDPテーブル
-    subtree_memo: Vec<T::Target>,
+    subtree_memo: Vec<M::Target>,
     /// 各頂点を根とした木全体のDPテーブル
-    ans: Vec<T::Target>,
+    ans: Vec<M::Target>,
     add_root: F,
 }
 
-impl<T: Monoid + Commutative, F: FnMut(&T::Target, usize, usize) -> T::Target> Rerooting<T, F> {
-    /// モノイド`T`は`add_root`によりできた「部分木+一辺」同士をmergeする関数を二項演算として持つ  
-    /// `add_root(subtree: T::Target, subtree_root: usize, new_root: usize) -> T::Target`  
-    /// 部分木に頂点 subtree_root → new_root の辺を追加する  
-    /// _monoidは型推論のためだけに使っています  
-    pub fn new(graph: &Vec<Vec<usize>>, add_root: F, _monoid: T) -> Self {
+impl<M: Monoid + Commutative, F: FnMut(&M::Target, usize, usize) -> M::Target> Rerooting<M, F> {
+    /// モノイド`M`は`add_root`によりできた「部分木+一辺」同士をmergeする関数を二項演算として持つ  
+    /// `add_root(subtree: &M::Target, subtree_root: usize, new_root: usize) -> M::Target`  
+    /// 部分木に頂点 `subtree_root → new_root` の辺を追加する  
+    /// `_monoid`は型推論のためだけに使っています  
+    pub fn new(graph: &Vec<Vec<usize>>, add_root: F, _monoid: M) -> Self {
         let vertex_cnt = graph.len();
-        let subtree_memo = vec![T::id_element(); vertex_cnt];
-        let ans = vec![T::id_element(); vertex_cnt];
+        let subtree_memo = vec![M::id_element(); vertex_cnt];
+        let ans = vec![M::id_element(); vertex_cnt];
         let mut ret = Self {
             vertex_cnt,
             subtree_memo,
@@ -30,11 +30,11 @@ impl<T: Monoid + Commutative, F: FnMut(&T::Target, usize, usize) -> T::Target> R
             add_root,
         };
         ret.dfs(graph, 0, usize::MAX);
-        ret.bfs(graph, 0, usize::MAX, T::id_element());
+        ret.bfs(graph, 0, usize::MAX, M::id_element());
         ret
     }
 
-    pub fn get_ans(&self, root: usize) -> T::Target {
+    pub fn get_ans(&self, root: usize) -> M::Target {
         assert!(root < self.vertex_cnt);
         self.ans[root].clone()
     }
@@ -46,11 +46,11 @@ impl<T: Monoid + Commutative, F: FnMut(&T::Target, usize, usize) -> T::Target> R
             }
             self.dfs(graph, to, v);
             let memo = (self.add_root)(&self.subtree_memo[to], to, v);
-            self.subtree_memo[v] = T::binary_operation(&self.subtree_memo[v], &memo);
+            self.subtree_memo[v] = M::binary_operation(&self.subtree_memo[v], &memo);
         }
     }
 
-    fn bfs(&mut self, graph: &Vec<Vec<usize>>, v: usize, p: usize, par_val: T::Target) {
+    fn bfs(&mut self, graph: &Vec<Vec<usize>>, v: usize, p: usize, par_val: M::Target) {
         // 左右から累積和を取っておく
         let mut buf = Vec::with_capacity(graph[v].len());
         for &to in &graph[v] {
@@ -60,26 +60,26 @@ impl<T: Monoid + Commutative, F: FnMut(&T::Target, usize, usize) -> T::Target> R
                 buf.push((self.add_root)(&self.subtree_memo[to], to, v));
             }
         }
-        let mut left_sum = vec![T::id_element(); buf.len() + 1];
-        let mut right_sum = vec![T::id_element(); buf.len() + 1];
+        let mut left_sum = vec![M::id_element(); buf.len() + 1];
+        let mut right_sum = vec![M::id_element(); buf.len() + 1];
         for i in 0..buf.len() {
-            left_sum[i + 1] = T::binary_operation(&left_sum[i], &buf[i]);
+            left_sum[i + 1] = M::binary_operation(&left_sum[i], &buf[i]);
         }
         for i in (0..buf.len()).rev() {
-            right_sum[i] = T::binary_operation(&buf[i], &right_sum[i + 1]);
+            right_sum[i] = M::binary_operation(&buf[i], &right_sum[i + 1]);
         }
         if p == usize::MAX {
             self.ans[v] = left_sum.last().unwrap().clone();
         } else {
-            self.ans[v] = T::binary_operation(left_sum.last().unwrap(), &par_val);
+            self.ans[v] = M::binary_operation(left_sum.last().unwrap(), &par_val);
         }
 
         // 子に伝播
         for (i, &to) in graph[v].iter().filter(|v| **v != p).enumerate() {
             // 一つも部分木をmergeしないなら、leafを用いる
-            let propagate = T::binary_operation(
+            let propagate = M::binary_operation(
                 &par_val,
-                &T::binary_operation(&left_sum[i], &right_sum[i + 1]),
+                &M::binary_operation(&left_sum[i], &right_sum[i + 1]),
             );
             let par_val = (self.add_root)(&propagate, v, to);
             self.bfs(graph, to, v, par_val);
