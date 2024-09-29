1c6dcc75270770c58dd8c4f00ca01eb77e35c445
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-09-29 20:55:20 +0900

    optimize select using additonal index

--- crates/succint/bitvec/src/lib.rs
+++ crates/succint/bitvec/src/lib.rs
@@ -1,9 +1,8 @@
 //! 完備辞書  
-//! 1.5N bit 用いているので、succintではない compactではある  
+//! 2N + O(1) bit 用いているので、succintではない compactではある  
 //! u64のブロックのみを使い、小ブロックは使わない  
 //! selectの高速化のために、x86_64の命令を(使えれば)使う  
-//! しかしselect用の索引を持たせてないので、selectはO(logN)  
-//! 雑なベンチマークによると、select1はrank1の4~5倍程度の時間がかかりそう
+//! 雑なベンチマークによると、select1はbit列がランダムなら平均でrank1の2倍程度の時間がかかりそう
 
 /// キャッシュ効率のため、ブロックとその前のブロックまでの1の数をまとめて持つ
 #[derive(Debug, Clone, Copy)]
@@ -17,6 +16,10 @@ pub struct BitVec {
     len: usize,
     blocks: Vec<Block>,
     all_popcnt: u32,
+    /// one_select[i] = 64i番目の1が属するブロックのindex
+    one_select: Vec<u32>,
+    /// zero_select[i] = 64i番目の0が属するブロックのindex
+    zero_select: Vec<u32>,
 }
 
 impl BitVec {
@@ -35,15 +38,29 @@ impl BitVec {
                 blocks[i >> 6].block |= 1 << (i & 63);
             }
         }
+        let all_popcnt = blocks.iter().map(|b| b.block.count_ones()).sum::<u32>() as usize;
         let mut popcnt = 0;
-        for b in blocks.iter_mut() {
+        let one_num = (all_popcnt + 63) / 64;
+        let zero_num = ((len - all_popcnt) + 63) / 64;
+        let mut one_select = Vec::with_capacity(one_num);
+        let mut zero_select = Vec::with_capacity(zero_num);
+        for (i, b) in blocks.iter_mut().enumerate() {
+            if popcnt as usize >= one_select.len() << 6 {
+                one_select.push(i as u32);
+            }
+            if (i << 6) - popcnt as usize >= zero_select.len() << 6 {
+                zero_select.push(i as u32);
+            }
             b.cum_sum_popcnt = popcnt;
             popcnt += b.block.count_ones();
         }
+        assert_eq!(popcnt as usize, all_popcnt);
         Self {
             len,
             blocks,
             all_popcnt: popcnt,
+            one_select,
+            zero_select,
         }
     }
 
@@ -64,14 +81,20 @@ impl BitVec {
         i - self.rank1(i)
     }
 
-    /// 0-basedでi番目の1の位置 O(logN)
+    /// 0-basedでi番目の1の位置 最悪O(logN) 平均O(1)
     pub fn select1(&self, i: usize) -> Option<usize> {
         if i >= self.all_popcnt as usize {
             return None;
         }
+        // ブロックで二分探索を行うが、その範囲は索引で絞る
+        // self.blocks[ok].cum_sum_popcnt <= i < self.blocks[ng].cum_sum_popcnt
+        let mut ok = self.one_select[(i >> 6).saturating_sub(1)] as usize;
+        let mut ng = if let Some(&ng) = self.one_select.get((i >> 6) + 1) {
+            ng as usize
+        } else {
+            self.blocks.len()
+        };
         let i = i as u32;
-        let mut ok = 0;
-        let mut ng = self.blocks.len();
         while ng - ok > 1 {
             let mid = (ok + ng) >> 1;
             if self.blocks[mid].cum_sum_popcnt <= i {
@@ -92,8 +115,12 @@ impl BitVec {
         if i >= all_0 {
             return None;
         }
-        let mut ok = 0;
-        let mut ng = self.blocks.len();
+        let mut ok = self.zero_select[(i >> 6).saturating_sub(1)] as usize;
+        let mut ng = if let Some(&ng) = self.zero_select.get((i >> 6) + 1) {
+            ng as usize
+        } else {
+            self.blocks.len()
+        };
         while ng - ok > 1 {
             let mid = (ok + ng) >> 1;
             if ((mid << 6) - self.blocks[mid].cum_sum_popcnt as usize) <= i {
@@ -139,40 +166,48 @@ mod test {
 
     #[test]
     fn test_rank() {
-        let mut rng = thread_rng();
-        const SIZE: usize = 250000;
-        let bool_vec = (0..SIZE).map(|_| rng.gen_bool(0.5)).collect::<Vec<_>>();
-        let bit_vec = BitVec::new(&bool_vec);
-        let mut ans1 = vec![0; SIZE + 1];
-        let mut ans0 = vec![0; SIZE + 1];
-        for i in 0..SIZE {
-            ans1[i + 1] = ans1[i] + bool_vec[i] as usize;
-            ans0[i + 1] = ans0[i] + !bool_vec[i] as usize;
+        fn test(size: usize) {
+            let mut rng = thread_rng();
+            let bool_vec = (0..size).map(|_| rng.gen_bool(0.5)).collect::<Vec<_>>();
+            let bit_vec = BitVec::new(&bool_vec);
+            let mut ans1 = vec![0; size + 1];
+            let mut ans0 = vec![0; size + 1];
+            for i in 0..size {
+                ans1[i + 1] = ans1[i] + bool_vec[i] as usize;
+                ans0[i + 1] = ans0[i] + !bool_vec[i] as usize;
+            }
+            for i in 0..size {
+                assert_eq!(bit_vec.rank1(i), ans1[i]);
+                assert_eq!(bit_vec.rank0(i), ans0[i]);
+            }
         }
-        for i in 0..SIZE {
-            assert_eq!(bit_vec.rank1(i), ans1[i]);
-            assert_eq!(bit_vec.rank0(i), ans0[i]);
+        for size in [0, 1, 63, 64, 65, 100, 1000, 10000, 100000, 250000] {
+            test(size);
         }
     }
 
     #[test]
     fn test_select() {
-        let mut rng = thread_rng();
-        const SIZE: usize = 250000;
-        let bool_vec = (0..SIZE).map(|_| rng.gen_bool(0.5)).collect::<Vec<_>>();
-        let bit_vec = BitVec::new(&bool_vec);
-        let mut one_indices = Vec::with_capacity(bit_vec.all_popcnt as usize);
-        let mut zero_indices = Vec::with_capacity(SIZE - bit_vec.all_popcnt as usize);
-        for i in 0..SIZE {
-            if bool_vec[i] {
-                one_indices.push(i);
-            } else {
-                zero_indices.push(i);
+        fn test(size: usize) {
+            let mut rng = thread_rng();
+            let bool_vec = (0..size).map(|_| rng.gen_bool(0.5)).collect::<Vec<_>>();
+            let bit_vec = BitVec::new(&bool_vec);
+            let mut one_indices = Vec::with_capacity(bit_vec.all_popcnt as usize);
+            let mut zero_indices = Vec::with_capacity(size - bit_vec.all_popcnt as usize);
+            for i in 0..size {
+                if bool_vec[i] {
+                    one_indices.push(i);
+                } else {
+                    zero_indices.push(i);
+                }
+            }
+            for i in 0..size {
+                assert_eq!(bit_vec.select1(i), one_indices.get(i).copied());
+                assert_eq!(bit_vec.select0(i), zero_indices.get(i).copied());
             }
         }
-        for i in 0..SIZE {
-            assert_eq!(bit_vec.select1(i), one_indices.get(i).copied());
-            assert_eq!(bit_vec.select0(i), zero_indices.get(i).copied());
+        for size in [0, 1, 63, 64, 65, 100, 1000, 10000, 100000, 250000] {
+            test(size);
         }
     }
 
@@ -201,14 +236,20 @@ mod test {
         };
         stop_watch();
         for &i in &rand_nums {
-            assert!(bit_vec.rank1(i) <= i);
+            let _ = bit_vec.rank1(i);
         }
         println!("rank1: {:.6}", stop_watch());
         for &i in &rand_nums {
-            if let Some(pos) = bit_vec.select1(i) {
-                assert_eq!(bit_vec.rank1(pos), i);
-            }
+            let _ = bit_vec.select1(i);
         }
-        println!("select1, rank1: {:.6}", stop_watch());
+        println!("select1: {:.6}", stop_watch());
+        for &i in &rand_nums {
+            let _ = bit_vec.rank0(i);
+        }
+        println!("rank0: {:.6}", stop_watch());
+        for &i in &rand_nums {
+            let _ = bit_vec.select0(i);
+        }
+        println!("select0: {:.6}", stop_watch());
     }
 }
