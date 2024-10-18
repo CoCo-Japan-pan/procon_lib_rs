437ba0bc32a4683512e2833fb9c993ea4ef01259
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-10-18 21:07:52 +0900

    modify enumerate invs and add test

--- crates/math/enumerate_inv_mods/src/lib.rs
+++ crates/math/enumerate_inv_mods/src/lib.rs
@@ -1,7 +1,9 @@
 use internal_modint::ModInt;
 
-/// [1, n)のmod逆元列挙を`O(n)`で行う (index 0には便宜的に0を入れておく)
+/// n以下のmod逆元列挙を`O(n)`で行う  
+/// index 0には便宜的に0を入れたn+1の長さの配列を返す
 pub fn enumerate_invs<M: ModInt>(n: usize) -> Vec<M> {
+    let n = n + 1;
     assert!(n <= M::modulus() as usize);
     let mut invs = vec![M::raw(0); n];
     if n <= 1 {
@@ -12,4 +14,18 @@ pub fn enumerate_invs<M: ModInt>(n: usize) -> Vec<M> {
         invs[i] = -invs[M::modulus() as usize % i] * M::raw(M::modulus() / i as u32);
     }
     invs
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test() {
+        use static_modint::ModInt998244353 as MInt;
+        const SIZE: usize = 1_000_000;
+        let invs = enumerate_invs::<MInt>(SIZE);
+        for i in 1..=SIZE {
+            assert_eq!(invs[i] * MInt::new(i), MInt::new(1));
+        }
+    }
 }
