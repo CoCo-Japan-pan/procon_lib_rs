8240679c1962dba7f84fa4e7d47d0a88c124227f
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-10-24 13:48:38 +0900

    add verify for floor_sum

---
+++ verify/yosupo/sum_of_floor_of_linear/src/main.rs
@@ -0,0 +1,15 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/sum_of_floor_of_linear
+
+use floor_sum::floor_sum;
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        t: usize,
+        n_m_a_b: [(i64, i64, i64, i64); t],
+    }
+    for (n, m, a, b) in n_m_a_b {
+        println!("{}", floor_sum(n, m, a, b));
+    }
+}
