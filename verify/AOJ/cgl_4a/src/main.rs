695a1f70ee13e562683af8783572c1c38ecee01b
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-10-24 23:55:12 +0900

    bug fix

--- verify/AOJ/cgl_4a/src/main.rs
+++ verify/AOJ/cgl_4a/src/main.rs
@@ -1,6 +1,6 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/CGL_4_A
 
-use convex_hull::monotone_chain;
+use convex_hull::{monotone_chain, Point};
 use proconio::{fastout, input};
 
 #[fastout]
@@ -9,7 +9,8 @@ fn main() {
         n: usize,
         x_y: [(i64, i64); n],
     }
-    let mut points = x_y.into_iter().map(|p| p.into()).collect::<Vec<_>>();
+    let mut points = x_y.into_iter().map(|p| p.into()).collect::<Vec<Point>>();
+    let start_point = *points.iter().min_by_key(|&&p| (p.y, p.x)).unwrap();
     points.sort_unstable();
     let hull = {
         let (mut lower_hull, mut upper_hull) = monotone_chain(&points, true);
@@ -19,7 +20,8 @@ fn main() {
         lower_hull
     };
     println!("{}", hull.len());
-    for p in hull {
-        println!("{}", p);
+    let start_id = hull.iter().position(|&p| p == start_point).unwrap();
+    for p in hull.iter().cycle().skip(start_id).take(hull.len()) {
+        println!("{} {}", p.x, p.y);
     }
 }
