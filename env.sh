5d6a50acfa188811e7e6dbc6e8f0ac6bcc43642a
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2024-10-29 16:40:17 +0900

    modify cargo generate template file

--- env.sh
+++ env.sh
@@ -1,7 +1,16 @@
+#!/bin/bash 
+
+# the directory of this script
+DIR="$(dirname "$BASH_SOURCE")"
+
 bundle_libs_atcoder() {
     cargo equip --remove docs --minify libs --exclude-atcoder-202301-crates --mine github.com/CoCo-Japan-pan | xsel -b
 }
 
 bundle_libs() {
     cargo equip --remove docs --minify libs --mine github.com/CoCo-Japan-pan | xsel -b
+}
+
+gen_template() {
+    cargo generate --path $DIR/template
 }
