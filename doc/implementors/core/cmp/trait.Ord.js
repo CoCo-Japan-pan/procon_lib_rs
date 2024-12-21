bf0557e1a9f8c2dfa3967e4a71e4532353db9acb
GitHub <noreply@github.com>
2024-12-21 09:20:07 +0000

    [auto-verifier] docs commit 63e59940df32b985fa783dc40d066f53dc9ba130

--- doc/implementors/core/cmp/trait.Ord.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"geometry_basics":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"geometry_basics/struct.Point.html\" title=\"struct geometry_basics::Point\">Point</a>"]],
-"multiset":[["impl&lt;K: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"multiset/struct.MultiSet.html\" title=\"struct multiset::MultiSet\">MultiSet</a>&lt;K&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
