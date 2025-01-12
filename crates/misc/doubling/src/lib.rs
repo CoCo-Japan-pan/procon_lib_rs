8c9c3391cb695d82b95a59d4623424d789b5d4c0
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2025-01-12 13:23:48 +0900

    add doubling

---
+++ crates/misc/doubling/src/lib.rs
@@ -0,0 +1,66 @@
+//! ダブリング  
+
+#[derive(Debug, Clone)]
+pub struct Doubling {
+    n: usize,
+    max_pow: u64,
+    table: Vec<Vec<usize>>,
+}
+
+impl Doubling {
+    /// [0, n) -> [0, n) の写像funcを受け取る  
+    /// 写像はmax_pow乗までのみ考える
+    pub fn new(func: &[usize], max_pow: u64) -> Self {
+        let n = func.len();
+        let mut table = vec![func.to_vec()];
+        let mut cur_pow = max_pow >> 1;
+        while cur_pow > 0 {
+            let mut next = vec![0; n];
+            let last_table = table.last().unwrap();
+            for i in 0..n {
+                next[i] = last_table[last_table[i]];
+            }
+            table.push(next);
+            cur_pow >>= 1;
+        }
+        Self { n, max_pow, table }
+    }
+
+    /// func^x(index)を返す
+    pub fn query(&self, mut index: usize, mut x: u64) -> usize {
+        assert!(index < self.n);
+        assert!(x <= self.max_pow);
+        let mut table_index = 0;
+        while x > 0 {
+            if x & 1 == 1 {
+                index = self.table[table_index][index];
+            }
+            table_index += 1;
+            x >>= 1;
+        }
+        index
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand::prelude::*;
+
+    #[test]
+    fn test() {
+        let mut rng = thread_rng();
+        const SIZE: usize = 1000;
+        let func = (0..SIZE)
+            .map(|_| rng.gen_range(0..SIZE))
+            .collect::<Vec<_>>();
+        let max_pow = 1000;
+        let doubling = Doubling::new(&func, max_pow);
+        for _ in 0..100 {
+            let index = rng.gen_range(0..SIZE);
+            let x = rng.gen_range(0..max_pow);
+            let expected = (0..x).fold(index, |i, _| func[i]);
+            assert_eq!(doubling.query(index, x as u64), expected);
+        }
+    }
+}
