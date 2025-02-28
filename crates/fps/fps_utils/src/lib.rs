0ec2602f830b030c3fa2f94fa7c80c8179b53d23
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2025-02-28 13:11:14 +0900

    impl bostan mori

--- crates/fps/fps_utils/src/lib.rs
+++ crates/fps/fps_utils/src/lib.rs
@@ -109,6 +109,42 @@ impl<T: ConvHelper> Fps<T> {
     }
 }
 
+/// `[x^n] p(x)/q(x)` を `O(k log k log n)` 時間で求める  
+/// `deg(p) < k, deg(q) = k, q[0] = 1` とする  
+/// [moriさんの記事](https://qiita.com/ryuhe1/items/da5acbcce4ac1911f47a#bostanmori-%E3%81%AE%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0-1)
+pub fn bostan_mori<T: ConvHelper>(p: &Fps<T>, q: &Fps<T>, mut n: u64) -> T {
+    assert!(!p.is_empty() && !q.is_empty());
+    assert!(p.len() < q.len());
+    assert_eq!(q.data[0].value(), 1);
+    let mut p = p.clone();
+    let mut q = q.clone();
+    while n > 0 {
+        let mut q_minus_x = q.clone();
+        for i in (1..q.len()).step_by(2) {
+            q_minus_x.data[i] = -q_minus_x.data[i];
+        }
+        let vx2 = q * &q_minus_x;
+        let vx = Fps::from(vx2.data.into_iter().step_by(2).collect::<Vec<T>>());
+        let pqminus = p * q_minus_x;
+        let u = if n % 2 == 0 {
+            Fps::from(pqminus.data.into_iter().step_by(2).collect::<Vec<T>>())
+        } else {
+            Fps::from(
+                pqminus
+                    .data
+                    .into_iter()
+                    .skip(1)
+                    .step_by(2)
+                    .collect::<Vec<T>>(),
+            )
+        };
+        p = u;
+        q = vx;
+        n >>= 1;
+    }
+    p.data[0] / q.data[0]
+}
+
 macro_rules! impl_ops {
     ($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident) => {
         impl<T: ConvHelper, S> $trait<S> for Fps<T>
@@ -180,5 +216,53 @@ impl<T: ConvHelper> Neg for Fps<T> {
     type Output = Fps<T>;
     fn neg(self) -> Self::Output {
         Fps::from(self.data.into_iter().map(|x| -x).collect::<Vec<T>>())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::prelude::*;
+    use static_modint::ModInt998244353 as MInt;
+
+    #[test]
+    fn test_bm_fib_all() {
+        const N: usize = 100000;
+        let naive = {
+            let mut fib = vec![MInt::new(0); N];
+            fib[1] += 1;
+            for i in 2..N {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            fib
+        };
+        let p = Fps::from(vec![MInt::new(0), MInt::new(1)]);
+        let q = Fps::from(vec![MInt::new(1), -MInt::new(1), -MInt::new(1)]);
+        for i in 0..N {
+            assert_eq!(bostan_mori(&p, &q, i as u64), naive[i]);
+        }
+    }
+
+    #[test]
+    fn test_bm_fib_big() {
+        use matrix::{Matrix, UsualSemiring};
+        type Rig = UsualSemiring<MInt>;
+        let mut rng = thread_rng();
+        fn use_matrix(n: u64) -> MInt {
+            if n == 0 {
+                return MInt::new(0);
+            }
+            let mat =
+                Matrix::<Rig>::from([[MInt::new(1), MInt::new(1)], [MInt::new(1), MInt::new(0)]]);
+            let vec = Matrix::from([[MInt::new(1)], [MInt::new(0)]]);
+            let res = mat.pow(n - 1) * &vec;
+            res.get(0, 0)
+        }
+        let p = Fps::from(vec![MInt::new(0), MInt::new(1)]);
+        let q = Fps::from(vec![MInt::new(1), -MInt::new(1), -MInt::new(1)]);
+        for _ in 0..1000 {
+            let n = rng.gen_range(10_u64.pow(5)..=10_u64.pow(18));
+            assert_eq!(bostan_mori(&p, &q, n), use_matrix(n));
+        }
     }
 }
